5a1203601707f710d446d1f56a2c1c0610afdf2d
tom2u <tbrodhead@2u.com>
2022-10-03 20:06:18 -0500

    initial commit

---
+++ main.tf
@@ -0,0 +1,52 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+#10.0.10.0/24
+variable "cidr_blocks" {
+  description = "cidr blocks and name tags for vpc and subnets"
+  type = list(object({
+    cidr_block = string
+    name = string
+  }))
+}
+
+variable avail_zone {}
+
+
+resource "aws_vpc" "development-vpc" {
+  cidr_block = var.cidr_blocks[0].cidr_block
+  tags = {
+    Name = var.cidr_blocks[0].name
+  }
+}
+
+resource "aws_subnet" "dev-subnet-1" {
+  vpc_id = aws_vpc.development-vpc.id
+  cidr_block = var.cidr_blocks[1].cidr_block
+  availability_zone = var.avail_zone
+  tags = {
+    Name = var.cidr_blocks[1].name
+  }
+}
+
+data "aws_vpc" "existing_vpc" {
+  default = true
+}
+
+resource "aws_subnet" "dev-subnet-2" {
+  vpc_id = data.aws_vpc.existing_vpc.id
+  cidr_block = "172.31.96.0/20" 
+  availability_zone = "us-east-1a"
+  tags = {
+    Name =  "subnet-2-default"
+  }
+}
+
+output "dev-vpc-id" {
+  value = aws_vpc.development-vpc.id
+}
+
+output "dev-subnet-id" {
+  value = aws_subnet.dev-subnet-1.id
+}
