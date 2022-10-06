8ecba9a5642c19b29b74aee7d8b3509beef061f6
tmb-github <brodhead.tom@gmail.com>
2022-10-06 16:00:47 -0500

    Through lesson 5

--- main.tf
+++ main.tf
@@ -9,6 +9,8 @@ variable env_prefix {}
 variable my_ip {}
 variable instance_type {}
 variable public_key_location {}
+variable private_key_location {}
+variable decrypted_private_key_location {}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
@@ -137,8 +139,29 @@ resource "aws_instance" "myapp-server" {
   associate_public_ip_address = true
   key_name = aws_key_pair.ssh-key.key_name
 
-  user_data = file("entry-script.sh")
+  #user_data = file("entry-script.sh")
+/*
+  connection {
+    type = "ssh"
+    host = self.public_ip
+    user = "ec2-user"
+    private_key = file(var.decrypted_private_key_location)
+    agent = false
+  } 
 
+  provisioner "file" {
+    source = "entry-script-on-ec2.sh"
+    destination = "/home/ec2-user/entry-script-on-ec2.sh"
+  }
+
+  provisioner "remote-exec" {
+    script = file("entry-script-on-ec2.sh")
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${self.public_ip} > output.txt"
+  }
+*/
   tags = {
     Name = "${var.env_prefix}-server"
   }
