6440969af65e3b3cacfdbacbd8edcefa13fc94aa
tmb-github <brodhead.tom@gmail.com>
2022-10-07 16:44:10 -0500

    final-section

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_version =">= 0.12"
+  backend "s3" {
+    bucket = "myapp-buckett"
+    key = "myapp/state.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
